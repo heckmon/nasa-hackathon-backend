75239d3c49e917dfef0f0a1493bc06d6b2f4d328
ATHUL A S <64469847+heckmon@users.noreply.github.com>
2025-10-03 14:00:00 +0530

    first commit

---
+++ server.py
@@ -0,0 +1,27 @@
+import os
+import requests
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from astroquery.jplhorizons import Horizons
+
+app = Flask(__name__)
+CORS(app, methods=['POST'])
+
+@app.route("/coordinate", methods=['POST'])
+def coordinates():
+    data = request.get_json()
+    try:
+        obj = Horizons(id=f'DES={data['id']}', location='500@399')
+        vectors = obj.vectors()
+        x = vectors['x'].value[0]
+        y = vectors['y'].value[0]
+        z = vectors['z'].value[0]
+        return jsonify({'x': x, 'y': y, 'z': z}), 200
+    except Exception as e: return f"An error occurred:\n{e}", 500
+
+@app.route("/near_items", methods=['POST'])
+def near_items():
+    response = requests.get(f"https://api.nasa.gov/neo/rest/v1/feed?api_key={os.getenv("NEOWS_API")}")
+    return jsonify(response.json())
+
+app.run(debug=True)
